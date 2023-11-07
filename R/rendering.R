0baf2ef424132bd0d9681fa246acdd229b104252
langbart <lorenzo.longobardi@gmail.com>
2023-11-07 13:15:11 +0100

    Move docs folder automatically

---
+++ R/rendering.R
@@ -0,0 +1,10 @@
+
+render_package <- function(path = "docs_book"){
+  bookdown::render_book(path)
+
+  docs_folder <- system.file("docs_book/docs", package = "Timor.nutrients")
+  path_new <- paste0(getwd(), "/docs")
+  file.rename(docs_folder, path_new)
+
+}
+
