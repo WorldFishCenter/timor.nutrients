534c4105e48695d23fe2103f48a96c55818faadb
langbart <lorenzo.longobardi@gmail.com>
2023-12-27 20:05:26 +0100

    Fix plot error

--- rand_analyses.R
+++ rand_analyses.R
@@ -114,3 +114,87 @@ treemap_gear_df %>%
   theme(legend.position = "bottom") +
   labs(fill = "")
 
+
+
+###### scores ######
+pars <- read_config()
+trips <- get_merged_trips(pars) %>% dplyr::filter(!is.na(landing_id))
+
+kobo_trips <-
+  trips %>%
+  dplyr::mutate(
+    landing_period = lubridate::floor_date(landing_date, unit = "month"),
+    landing_id = as.character(landing_id),
+    n_fishers = fisher_number_man + fisher_number_woman + fisher_number_child
+  ) %>%
+  tidyr::unnest(.data$landing_catch) %>%
+  tidyr::unnest(.data$length_frequency) %>%
+  dplyr::filter(!is.na(.data$weight)) %>%
+  dplyr::group_by(.data$landing_id, .data$landing_period) %>%
+  dplyr::summarise(
+    landing_id = dplyr::first(landing_id),
+    reporting_region = dplyr::first(reporting_region),
+    landing_date = dplyr::first(landing_date),
+    trip_duration = dplyr::first(trip_duration),
+    n_fishers = dplyr::first(n_fishers),
+    habitat = dplyr::first(habitat),
+    gear_type = dplyr::first(gear_type),
+    mesh_size = dplyr::first(mesh_size),
+    vessel_type = dplyr::first(vessel_type),
+    landing_value = dplyr::first(landing_value),
+    dplyr::across(
+      c(.data$weight:.data$Vitamin_A_mu),
+      ~ sum(.x) / 1000
+    )
+  ) %>%
+  dplyr::ungroup()
+
+
+nutrient_score <-
+  trips %>%
+  dplyr::mutate(
+    landing_period = lubridate::floor_date(landing_date, unit = "month"),
+    landing_id = as.character(landing_id),
+    n_fishers = fisher_number_man + fisher_number_woman + fisher_number_child
+  ) %>%
+  tidyr::unnest(.data$landing_catch) %>%
+  tidyr::unnest(.data$length_frequency) %>%
+  dplyr::filter(!is.na(.data$weight),
+                catch_taxon %in% c("CLP", "GZP", "TUN", "SDX", "FLY")) %>% #, "CGX", "MOO", "SNA", "CJX", "BEN", "RAX", "LWX")) %>%
+  dplyr::group_by(.data$landing_id, catch_taxon) %>%
+  dplyr::summarise(
+    landing_value = dplyr::first(landing_value),
+    dplyr::across(
+      c(.data$weight:.data$Vitamin_A_mu),
+      ~ sum(.x) / 1000
+    )
+  ) %>%
+  dplyr::ungroup()
+
+we <-
+nutrient_score %>%
+  rename_nutrients_mu() %>%
+  tidyr::pivot_longer(-c(landing_id, catch_taxon, landing_value, weight), names_to = "nutrient") %>%
+  dplyr::left_join(timor.nutrients::RDI_tab) %>%
+  dplyr::mutate(
+    nutrients_kg_per_kg = value / weight, # standardize nutrients for 1 kg of catch
+    nutrients_g_per_kg = nutrients_kg_per_kg * 1000, # convert stand nutrients in grams
+    people_rni_kg = (nutrients_g_per_kg / conv_factor) * 10
+  ) %>%
+  dplyr::select(-c(conv_factor, nutrients_kg_per_kg, nutrients_g_per_kg)) %>%
+  dplyr::filter(!nutrient == "selenium") %>%
+  dplyr::group_by(landing_id, catch_taxon) %>%
+  dplyr::summarise(n_score = sum(people_rni_kg),
+                   landing_value = median(landing_value)) %>%
+  dplyr::group_by(catch_taxon) %>%
+  dplyr::mutate(n = dplyr::n()) %>%
+  dplyr::group_by(catch_taxon) %>%
+  dplyr::summarise(n = dplyr::first(n),
+                   n_score = dplyr::first(n_score),
+                   landing_value = median(landing_value, na.rm = T))
+
+we %>%
+  ggplot(aes(n_score, landing_value, alpha = log(n), color = catch_taxon)) +
+  theme_minimal() +
+  geom_point(size = 5)
+
