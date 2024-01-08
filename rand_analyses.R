434a57d52cc6e8df901a527deac38dc39f299d48
langbart <lorenzo.longobardi@gmail.com>
2024-01-08 20:21:28 +0100

    Add habitat nutritional values plot

--- rand_analyses.R
+++ rand_analyses.R
@@ -1,118 +1,56 @@
 ### descriptive nutrients plots ####
-treemap_habitat_df <-
+habitat_nutrients <-
   timor.nutrients::kobo_trips %>%
-  dplyr::filter(weight > 0) %>%
-  dplyr::select(habitat, weight, Selenium_mu:Vitamin_A_mu) %>%
-  na.omit() %>%
+  dplyr::select(habitat, weight:Vitamin_A_mu) %>%
   rename_nutrients_mu() %>%
-  tidyr::pivot_longer(-c(habitat, weight),
-    names_to = "nutrient", values_to = "concentration_g"
+  tidyr::pivot_longer(-c(habitat, weight), names_to = "nutrient") %>%
+  dplyr::left_join(RDI_tab) %>%
+  dplyr::mutate(
+    value = value / weight,
+    inds_kg = (value * 1000) / conv_factor
   ) %>%
-  dplyr::mutate(concentration_g_stand = concentration_g / weight) %>%
-  dplyr::select(-weight) %>%
-  dplyr::select(-concentration_g) %>%
   dplyr::group_by(habitat, nutrient) %>%
-  dplyr::summarise(concentration_g_stand = median(concentration_g_stand)) %>%
-  dplyr::ungroup() %>%
-  dplyr::left_join(timor.nutrients::RDI_tab, by = "nutrient") %>%
+  dplyr::summarise(inds_kg = median(inds_kg, na.rm = T) / 10) %>%
+  dplyr::mutate(inds_kg = inds_kg * 100) %>%
+  dplyr::filter(!nutrient == "selenium") %>%
   dplyr::mutate(
-    concentration_g_stand = concentration_g_stand * 1000,
     nutrient = stringr::str_to_title(nutrient),
     nutrient = dplyr::case_when(
-      nutrient %in% c("Selenium", "Vitamina") ~ paste(nutrient, "(μg)"),
-      nutrient %in% c("Calcium", "Iron", "Zinc") ~ paste(nutrient, "(mg)"),
-      TRUE ~ paste(nutrient, "(g)")
-    ),
-    nutrient = ifelse(nutrient == "Vitamina (μg)", "Vitamin-A (μg)", nutrient),
-    nutrient = ifelse(nutrient == "Omega3 (g)", "Omega-3 (g)", nutrient)
-  ) %>%
-  dplyr::mutate(concentration_g_stand = (concentration_g_stand / conv_factor)) %>%
-  dplyr::filter(!nutrient == "Selenium (μg)") %>%
-  dplyr::mutate(nutrient = factor(nutrient, levels = c(
-    "Protein (g)",
-    "Zinc (mg)",
-    "Calcium (mg)",
-    "Omega-3 (g)",
-    "Vitamin-A (μg)",
-    "Iron (mg)"
-  )))
+      nutrient == "Omega3" ~ "Omega-3",
+      nutrient == "Vitamina" ~ "Vitamin-A",
+      TRUE ~ nutrient
+    )
+  )
 
-treemap_habitat_df %>%
-  ggplot(aes(
-    area = concentration_g_stand,
-    fill = nutrient,
-    label = habitat,
-    subgroup = nutrient
-  ), alpha = 0.5) +
-  geom_treemap() +
-  geom_treemap_subgroup_border() +
-  geom_treemap_subgroup_text(
-    place = "centre",
-    grow = T,
-    alpha = 0.5,
-    colour = "black",
-    fontface = "italic",
-    min.size = 0
+ggplot2::ggplot() +
+  ggplot2::theme_minimal() +
+  ggchicklet::geom_chicklet(habitat_nutrients,
+                            mapping = ggplot2::aes(
+                              y = inds_kg,
+                              x = reorder(habitat, inds_kg),
+                              fill = nutrient
+                            ),
+                            position = ggplot2::position_stack(reverse = FALSE),
+                            alpha = 0.8,
+                            width = 0.8
   ) +
-  geom_treemap_text(colour = "white", place = "topleft", reflow = T) +
-  scale_fill_viridis_d(direction = -1) +
-  theme(legend.position = "bottom") +
-  labs(fill = "")
-
-
-####
-
-treemap_gear_df <-
-  timor.nutrients::kobo_trips %>%
-  dplyr::filter(weight > 0) %>%
-  dplyr::select(gear_type, weight, Selenium_mu:Vitamin_A_mu) %>%
-  na.omit() %>%
-  rename_nutrients_mu() %>%
-  tidyr::pivot_longer(-c(gear_type, weight),
-    names_to = "nutrient", values_to = "concentration_g"
-  ) %>%
-  dplyr::mutate(concentration_g_stand = concentration_g / weight) %>%
-  dplyr::select(-weight) %>%
-  dplyr::select(-concentration_g) %>%
-  dplyr::group_by(gear_type, nutrient) %>%
-  dplyr::summarise(concentration_g_stand = median(concentration_g_stand)) %>%
-  dplyr::ungroup() %>%
-  dplyr::left_join(timor.nutrients::RDI_tab, by = "nutrient") %>%
-  dplyr::mutate(
-    concentration_g_stand = concentration_g_stand * 1000,
-    nutrient = stringr::str_to_title(nutrient),
-    nutrient = dplyr::case_when(
-      nutrient %in% c("Selenium", "Vitamina") ~ paste(nutrient, "(μg)"),
-      nutrient %in% c("Calcium", "Iron", "Zinc") ~ paste(nutrient, "(mg)"),
-      TRUE ~ paste(nutrient, "(g)")
-    ),
-    nutrient = ifelse(nutrient == "Vitamina (μg)", "Vitamin-A (μg)", nutrient),
-    nutrient = ifelse(nutrient == "Omega3 (g)", "Omega-3 (g)", nutrient)
-  ) %>%
-  dplyr::mutate(concentration_g_stand = (concentration_g_stand / conv_factor)) %>%
-  dplyr::filter(!nutrient == "Selenium (μg)")
-
-treemap_gear_df %>%
-  ggplot(aes(
-    area = concentration_g_stand,
-    fill = nutrient,
-    label = gear_type,
-    subgroup = nutrient
-  ), alpha = 0.5) +
-  geom_treemap() +
-  geom_treemap_subgroup_border() +
-  geom_treemap_subgroup_text(
-    place = "centre",
-    grow = T,
-    alpha = 0.5,
-    colour = "black",
-    fontface = "italic",
-    min.size = 0
+  ggplot2::geom_text(habitat_nutrients,
+            mapping = ggplot2::aes(
+              y = inds_kg,
+              x = reorder(habitat, inds_kg),
+              label = round(inds_kg, 1),
+              group = nutrient
+            ),
+            position = ggplot2::position_stack(0.5, reverse = FALSE),
+            color = "white",
+            size = 3
   ) +
-  geom_treemap_text(colour = "white", place = "topleft", reflow = T) +
-  scale_fill_viridis_d(direction = -1) +
-  theme(legend.position = "bottom") +
-  labs(fill = "")
+  ggplot2::scale_fill_manual(values = timor.nutrients::palettes$nutrients_palette) +
+  ggplot2::scale_color_manual(values = timor.nutrients::palettes$nutrients_palette) +
+  ggplot2::scale_y_continuous(labels = scales::label_percent(scale = 1)) +
+  ggplot2::coord_flip(expand = FALSE) +
+  ggplot2::theme(legend.position = "bottom") +
+  ggplot2::labs(x = "", fill = "", y = "Matched RNI from 100g portion")
 
 
 
