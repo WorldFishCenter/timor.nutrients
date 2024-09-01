4d7678d235227bfdcd7878ad51ac9803f049aa62
langbart <lorenzo.longobardi@gmail.com>
2024-09-01 11:49:14 +0200

    Update report

--- R/rendering.R
+++ R/rendering.R
@@ -1,6 +1,6 @@
-render_docs <- function(path = "docs_book") {
-  bookdown::render_book(path)
- #bookdown::render_book("docs_book", "bookdown::pdf_book")
+render_docs <- function(path = "docs_book", output = NULL) {
+
+  bookdown::render_book(path, output_format = output)
 
   docs_folder <- system.file("docs_book/docs", package = "timor.nutrients")
   path_new <- paste0(getwd(), "/docs")
